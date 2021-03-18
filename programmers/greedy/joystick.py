943a998ac53bb8b888c8b77e2693db4531057739
kunholee_98 <dlrjsgh750@naver.com>
2021-03-18 23:18:46 +0900

    조이스틱

--- programmers/greedy/joystick.py
+++ programmers/greedy/joystick.py
@@ -22,16 +22,35 @@ def solution(name):
         else:
             cnt += dif
         print("here",cnt)
-    # 이동하는 방법에 대해서
-    j = 0
-    #while 0 in way:
-     #   way[j] = 1
-      #  for i in range(len(way)):
-       #     if way[i] == 0 and abs(j-i) < 10:pass
-    #for i in range(len(result)):
-    #    result[i] = chr(result[i]+64)
-    #print(result)
-    print(cnt-1)
-    
-    answer = 0
-    return answer
+
+    # 커서를 이동하는 횟수 카운팅
+    # j는 커서의 현 위치를 나타냄 (시작점)
+    j=0
+    print("cnt",cnt)
+    # 1이 A 혹은 이미 바꾼 알파벳을 의미하므로 더이상 바꿀 문자가 없을 때까지 while 문 반복
+    while 0 in way:
+        print(way)
+        # 현위치를 다녀갔음을 표시
+        way[j] = 1
+        # 좌(up), 우(down)로 1을 더하고 빼면서 커서를 양방향으로 움직이며 0이 저장되어 있는 가까운 거리의 index를 찾음
+        # 이때 움직인 만큼 cnt에 1씩 더해주어 커서의 움직임을 카운팅
+        up, down = j,j
+        go = True
+        while go:
+            print("up or down", up, down)
+            print("up",way[up],"down",way[down])
+            up +=1
+            down -= 1
+            cnt +=1
+            if up != j and (way[up] == 0 or way[down] == 0):
+                go = False
+        if way[up] == 0:
+            way[up] = 1
+            j = up
+        elif way[down] == 0:
+            way[down] = 1
+            j = down
+    print(cnt)
+    return cnt
+
+solution(list(map(str,input(">> ").split())))
