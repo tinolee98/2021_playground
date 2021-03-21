a25e51c619cf4cbe063f67d0300d44953501cac0
kunholee_98 <dlrjsgh750@naver.com>
2021-03-21 23:22:09 +0900

    DS HW01 - class shape should be 'interface class'

---
+++ C++_practice/HW01/HW1_class.h
@@ -0,0 +1,105 @@
+// HW1_driver.cpp에 사용되는 class를 선언해놓은 헤더파일
+// shape class는 interface class(destructor & pure virtual function으로만 이루어진 class)여야하므로 다시 짜야함..!
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+const double PI = 3.141592;
+
+class Shape {
+    public:
+        double radius;  // for circle
+        double x,y;     // for two & three dimensional shapes
+        double z;       // for three dimensional shapes
+        Shape(double _radius) : radius(_radius) {}                      // for circle & sphere
+        Shape(double _x, double _y) : x(_x), y(_y) {}                   // for rectangle
+        Shape(double _x, double _y, double _z) : x(_x), y(_y), z(_z) {} // for cubiod     
+        virtual ~Shape() {}
+        virtual void print()
+        {
+            cout << "Shape" << endl;
+        }
+        virtual double getArea() { return -1; }
+        virtual double getVolume() { return -1; }
+};
+
+class TwoDimensionalShape : public Shape {
+    public:
+        virtual void print()
+        {
+            Shape::print();
+            cout << "TwoDimesional" << endl;
+        }
+        TwoDimensionalShape(double _radius) : Shape(_radius) {}
+        TwoDimensionalShape(double _x, double _y) : Shape(_x, _y) {}
+        virtual double getArea () { return -1; }
+};
+
+class ThreeDimensionalShape : public Shape {
+    public:
+        virtual void print()
+        {
+            Shape::print();
+            cout << "ThreeDimensional" << endl;
+        }
+        ThreeDimensionalShape(double _radius) : Shape(_radius) {}
+        ThreeDimensionalShape(double _x, double _y, double _z) : Shape(_x, _y, _z) {}
+        virtual double getVolume () { return -1; }
+};
+
+class Rectangle : public TwoDimensionalShape {
+    public:
+        void print() {
+            TwoDimensionalShape::print();
+            cout << "Rectangle" << endl;
+        }
+        Rectangle(double _x, double _y) : TwoDimensionalShape(_x, _y) {}
+        double getArea()
+        {
+            cout << "( x : " << x << ", y : " << y << " )"<< endl;
+            return x * y;
+        }
+};
+
+class Circle : public TwoDimensionalShape {
+    public:
+        void print() {
+            TwoDimensionalShape::print();
+            cout << "Circle" << endl;
+        }
+        Circle(double _radius) : TwoDimensionalShape(_radius) {}
+        double getArea()
+        {
+            cout << "( radius : " << radius << " )" << endl;
+            return radius * 2 * PI;
+        }
+};
+
+class Cuboid : public ThreeDimensionalShape {
+    public:
+        void print() {
+            ThreeDimensionalShape::print();
+            cout << "Cuboid" << endl;
+        }
+        Cuboid(double _x, double _y, double _z) : ThreeDimensionalShape(_x,_y,_z) {}
+        double getVolume()
+        {
+            cout << "( x : " << x << ", y : " << y << ", z : " << z << " )"<< endl;
+            return x * y * z;
+        }  
+};
+
+class Sphere : public ThreeDimensionalShape {
+    public:
+        void print() {
+            ThreeDimensionalShape::print();
+            cout << "Sphere" << endl;
+        }
+        Sphere(double _radius) : ThreeDimensionalShape(_radius) {}
+        double getVolume()
+        {
+            cout << "( radius : " << radius << " )"<<endl;
+            return 4/3 * PI * pow(radius,3);
+        }
+};
---
+++ C++_practice/HW01/HW1_driver.cpp
@@ -0,0 +1,36 @@
+/* HW1. Inheritance Hierarchy of shapes. shape - two-dimensional or three-dimensional - some shapes 순으로 상속되는 클래스들 다음의 조건을 만족하도록 만들어라.
+1. shape class 는 interfaces을 포함하는 abstract base class이다.
+2. TwoDimensionalShape, ThreeDimnesionalShape는 shape를 derieved하여 만들어진 abstract class이다.
+3. 각 class의 type과 dimensions를 output으로 갖는 virtual print function을 만들어라.
+4. area, volume 을 출력하는 virtual function을 만들어라.
+5. shape class hierarchy를 test할 수 있는 driver program을 만들어라. */
+
+// Driver_program
+#include<iostream>
+#include<cmath>
+#include "HW1_class.h"
+using namespace std;
+int main()
+{
+    double radius = 2.0;
+    double x = 4.0;
+    double y = 5.0;
+    double z = 6.0;
+    // 각 class를 호출하고, area, volume을 출력할 수 있는 driver program 만드는 중.
+    Shape* rect = new Rectangle(x,y);
+    Shape* cir = new Circle(radius);
+    Shape* cub = new Cuboid(x,y,z);
+    Shape* sph = new Sphere(radius);
+
+    rect->print();
+    cout << "Area of Rectangle " << rect->getArea() << endl<<endl;
+
+    cir->print();
+    cout << "Area of Circle " << cir->getArea() << endl<<endl;
+
+    cub->print();
+    cout << "Volume of Cuboid " << cub->getVolume() << endl<<endl;
+
+    sph->print();
+    cout << "Volume of Sphere " << sph->getVolume() << endl<<endl;
+}
