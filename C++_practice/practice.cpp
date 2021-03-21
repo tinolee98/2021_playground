3edebef3458115d2fb152cd293c375f2676c25d6
kunholee_98 <dlrjsgh750@naver.com>
2021-03-21 21:51:25 +0900

    HW1 작성중

---
+++ C++_practice/practice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char name[10];
+    std::cout << "hello world!" << std::endl;
+    std::cin >> name;
+    std::cout << name << std::endl;
+    return 0;
+}
