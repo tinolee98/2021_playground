cf8d99b1e5cdce4b2ade551a87ab3c37a869e192
kunholee_98 <dlrjsgh750@naver.com>
2021-04-25 23:18:17 +0900

    javascript 연습

---
+++ javascript/myapp/practice.js
@@ -0,0 +1,40 @@
+var userID = 1;
+var userName = 'Lee';
+var result = 1+2; 
+var user = { id:1, name: 'Lee'};
+var users = [
+    {id:1, name: 'Lee'},
+    {id :2, name: 'Kim'}
+];
+
+console.log("ab" + "asdf" + "asdf");
+
+
+var expression = 1;
+
+switch (expression) {
+    case 1:
+        console.log(`${expression}, ${typeof expression}`);
+        //만약 break가 없다면.
+    case 10:
+        console.log("case 10");
+        break;
+    default:
+        console.log("default");
+}
+
+foo: {
+    console.log("foo");
+    break foo;
+    console.log("somebody helpme!"); // 어차피 없는 친구다 이말이야.
+}
+console.log("I'm out")
+
+outer: for(var i=0;i<3;i++){
+    inner:for(var j=0;j<3;j++){
+        if (i+j === 3) break inner;
+        console.log(`i ${i}, j ${j}`);
+    }
+}
+console.log("done!");
+console.log(Array+'');
