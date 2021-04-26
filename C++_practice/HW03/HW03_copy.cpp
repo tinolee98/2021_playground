11e85a02db217b03963560db80123e9bcd874aa6
kunholee98 <dlrjsgh750@naver.com>
2021-04-26 16:18:55 +0900

    hw03 해결!

--- C++_practice/HW03/HW03_copy.cpp
+++ C++_practice/HW03/HW03_copy.cpp
@@ -3,6 +3,7 @@
 #include<cmath>
 #include<stack>
 #include<typeinfo>
+//#include "HW03.h"
 using namespace std;
 
 static stack<int> opStack;
@@ -25,45 +26,65 @@ void inputFunc(char _input){
     temp = findIndex(_input);
     if(temp == 7){
         result += _input;
+        cout << "add operand in inputFunc" << endl;
+        cout << result << endl;
     }
     else {
         temp = findIndex(_input);
         opStack.push(temp);
+        cout << "add opStack in inputFunc" <<opStack.top() << endl;
+        cout << "opIndex" <<opStack.top() << endl;
     }
 }
 
 void infix2postfix (string _input) {
+    // now는 opStack 내의 operation을 꺼내야하는지, 쌓아야하는지 알려주는 boolean marker
     int top;
     int current;
     for(int i=0; i < _input.length(); i++){
+        cout << endl << endl<< "result is " << result << " and i is " << i << endl;
+        cout << "opStack size is " << opStack.size() << endl;
         top = !opStack.empty() ? opStack.top() : 0;
+        cout << "top is " << top << endl;
         current = findIndex(_input[i]);
+        cout << "current is " << current << endl;
         if(current == 7){
             inputFunc(_input[i]);
+            cout << "add operand" << endl;
         }
         else if(current != 3){
+            cout << "input is operator which is not )" << endl;
             if(current%5 >= top%5 || top == 4 || opStack.empty()){
+                cout << "input's prior is higher or top is (" << endl;
                 inputFunc(_input[i]);  
                 continue;
             }
             while(opStack.top() != 4 && !opStack.empty()){
+                cout << "top's prior is higher and repeat pop&push" << endl;
+                cout << "opStack.size = " <<opStack.size() << endl;
                 top = opStack.top();
                 opStack.pop();
                 result += op[top];
                 if(opStack.empty()) break;                
             }
+            cout << "pop&push completed" << endl;
+            cout<< "result is " << result << endl;
             inputFunc(_input[i]);
         }
         else{
+            cout << "input is )" << endl;
             while(top != 4){
+                cout << "pop&push to find (" << endl;
                 opStack.pop();
                 result += op[top];
                 top = opStack.top();
             }
+            cout << "we found ( and pop it!" << endl;
             opStack.pop();
         }
     }
     while(!opStack.empty()){
+        cout << "input string is empty. thus, opStack.empty" << endl;
         top = opStack.top();
         opStack.pop();
         result += op[top];
