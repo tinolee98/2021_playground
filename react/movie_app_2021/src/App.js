59eaa6b49c4786c8edbc8256288c648e82e0400a
kunholee_98 <dlrjsgh750@naver.com>
2021-08-05 12:37:10 +0900

    react 연습

--- react/movie_app_2021/src/App.js
+++ react/movie_app_2021/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from "react";
+import axios from "axios";
+
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: [] 
+  };
+  componentDidMount() {
+    axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }
+  render(){
+    const {isLoading} = this.state;
+    return (
+    <div>
+      {isLoading ? "Loading..." : "We are ready!"}
+      <h1>the number is {this.state.count}</h1>
+      <button onClick={this.plus}>Plus</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
-  );
+    );
+  }
 }
+
+
 
 export default App;
