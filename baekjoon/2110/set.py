0d3a6a95f3cf5d3f3fdeee3d27d723e6338362df
kunholee_98 <dlrjsgh750@naver.com>
2021-10-22 18:08:48 +0900

    집합 수정

--- baekjoon/2110/set.py
+++ baekjoon/2110/set.py
@@ -5,36 +5,27 @@ input = sys.stdin.readline
 Set = [0 for _ in range(21)]
 for _ in range(N):
     cmd = list(input().rstrip().split())
-    if cmd[0] == "add":
-        cmd[0] = 0
-    elif cmd[0] == "remove":
-        cmd[0] = 1
-    elif cmd[0] == "check":
-        cmd[0] = 2
-    elif cmd[0] == "toggle":
-        cmd[0] = 3
-    elif cmd[0] == "all":
-        cmd[0] = 4
-    elif cmd[0] == "empty":
-        cmd[0] = 5
-
     if len(cmd) == 1:
-        if cmd[0] == 4:
+        if cmd[0] == "all":
             Set = [1 for _ in range(21)]
         else:
             Set = [0 for _ in range(21)]
         continue
     c,n = cmd
     n = int(n)
-    if c == 0:
+    if c == "add":
         Set[n] = 1
-    elif c == 1:
+    elif c == "remove":
         Set[n] = 0
-    elif c == 2:
-        r = 1 if Set[n] == 1 else 0
-        print(r)
-    elif c == 3:
-        if Set[n] == 0:
-            Set[n] == 1
+    elif c == "check":
+        print(1 if Set[n]==1 else 0)
+    elif c == "toggle":
+        if Set[n] == 1:
+            Set[n] = 0 
         else:
-            Set[n] == 0
+            Set[n] = 1
+
+
+#for cmd in cmds:
+    #print(cmd)
+    #print(Set)
